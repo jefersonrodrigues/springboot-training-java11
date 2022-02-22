1e17bad75b00667c067abd9baa168d70c15bd6cb
unknown <jeferson.silva@ecomp.ufsm.br>
2022-02-22 14:18:51 -0300

    Payment, one-to-one association

--- src/main/java/com/rodrigues/jeferson/basic/config/TestConfig.java
+++ src/main/java/com/rodrigues/jeferson/basic/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rodrigues.jeferson.basic.entities.Category;
 import com.rodrigues.jeferson.basic.entities.Order;
 import com.rodrigues.jeferson.basic.entities.OrderItem;
+import com.rodrigues.jeferson.basic.entities.Payment;
 import com.rodrigues.jeferson.basic.entities.Product;
 import com.rodrigues.jeferson.basic.entities.User;
 import com.rodrigues.jeferson.basic.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
